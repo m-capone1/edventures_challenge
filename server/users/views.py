7cfbf3060b8c9030456894702e2cc130b2012321
Maddy Capone <maddycapone@gmail.com>
2024-10-13 14:21:08 -0400

    add routes for CRUD in server

--- server/users/views.py
+++ server/users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import UserProfile
+from .serializers import UserProfileSerializer
 
-# Create your views here.
+class UserProfileListCreate(generics.ListCreateAPIView):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+
+class UserProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
